586f2f2b1d6544cb3bed3bc4fc78bf79e796bbc5
IvashenkoSasha <124379919+IvashenkoSasha@users.noreply.github.com>
2023-06-24 22:37:07 +0300

    do a work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const list = document.querySelector('.gallery');
+
+const markup = galleryItems.map(createMarkup).join('');
+list.insertAdjacentHTML('beforeend', markup);
+
+list.addEventListener('click', openModal);
+
+let instance;
+
+function createMarkup(param) {
+    return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${param.original}">
+    <img
+      class="gallery__image"
+      src="${param.preview}"
+      data-source="${param.original}"
+      alt="${param.description}"
+    />
+  </a>
+</li>
+    `;
+};
+
+function openModal(evt) {
+    evt.preventDefault();
+    const target = evt.target;
+    const img = target.dataset.source;
+    const alt = target.alt
+
+    instance = basicLightbox.create(`<img src="${img}" alt="${alt}">`,
+        {
+            onShow: (instance) => {
+                window.addEventListener('keydown', modalEscClose);
+            },
+            onClose: (instance) => {
+                window.removeEventListener('keydown', modalEscClose);
+            }
+    })
+    instance.show();
+};
+
+function modalEscClose(evt) {
+        if (evt.code !== 'Escape') {
+            return;
+    };
+        if (!instance.visible()) {
+            return;
+        };
+        instance.close();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const list = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = galleryItems.map(createMarkup).join('');
+list.insertAdjacentHTML('beforeend', markup);
+
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
+
+function createMarkup(param) {
+    return `
+<li class="gallery__item">
+   <a class="gallery__link" href="${param.original}">
+      <img class="gallery__image" src="${param.preview}" alt="${param.description}" />
+   </a>
+</li>
+    `;
+};
